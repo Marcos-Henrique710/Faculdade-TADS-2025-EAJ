2282a270bca2c7de0194e0c109c97d6b5707d96b
Marcos_Henrique710 <marcoshenriqueufrn@gmail.com>
2025-08-14 23:41:33 -0300

    Atividade 2 - Aula 1 - Elevador

--- 2° periodo/programação orientada a objetos/atividades/atividades poo/atv2_aula2_Elevador/Elevador_atv2_aula1/src/App.java
+++ 2° periodo/programação orientada a objetos/atividades/atividades poo/atv2_aula2_Elevador/Elevador_atv2_aula1/src/App.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Elevador novoElevador = new Elevador(10, 5); // 10 andares, capacidade para 5 pessoas
+
+        int opcao;
+        do {
+            System.out.println("\n=== Sistema de Controle do Elevador ===");
+            System.out.println("1. Entrar no elevador");
+            System.out.println("2. Subir/Descer para um andar");
+            System.out.println("3. Sair do elevador");
+            System.out.println("4. Verificar status");
+            System.out.println("5. Sair do programa");
+            System.out.print("Digite a opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    novoElevador.entrar();
+                    break;
+                case 2:
+                    System.out.print("Digite o andar desejado: ");
+                    int andar = scanner.nextInt();
+                    novoElevador.mover(andar);
+                    break;
+                case 3:
+                    novoElevador.sair();
+                    break;
+                case 4:
+                    novoElevador.status();
+                    break;
+                case 5:
+                    System.out.println("Encerrando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 5);
+
+        scanner.close();
     }
 }
--- 2° periodo/programação orientada a objetos/atividades/atividades poo/atv2_aula2_Elevador/Elevador_atv2_aula1/src/Elevador.java
+++ 2° periodo/programação orientada a objetos/atividades/atividades poo/atv2_aula2_Elevador/Elevador_atv2_aula1/src/Elevador.java
@@ -1,3 +1,66 @@
 public class Elevador {
-    
+    int andarAtual;
+    int totalAndares;
+    int capacidade;
+    int pessoasPresentes;
+
+    public Elevador(int totalAndares, int capacidade) {
+        this.andarAtual = 0; // térreo
+        this.totalAndares = totalAndares;
+        this.capacidade = capacidade;
+        this.pessoasPresentes = 0;
+    }
+
+    public void entrar() {
+        if (pessoasPresentes < capacidade) {
+            pessoasPresentes++;
+            System.out.println("Uma pessoa entrou. Total de pessoas: " + pessoasPresentes);
+        } else {
+            System.out.println("Elevador cheio!");
+        }
+    }
+
+    public void mover(int andarDestino) {
+        if (andarDestino < 0 || andarDestino >= totalAndares) {
+            System.out.println("Andar inválido!");
+            return;
+        }
+        if (andarDestino == andarAtual) {
+            System.out.println("Você já está no andar " + andarAtual);
+            return;
+        }
+
+        System.out.println("Indo do andar " + andarAtual + " para o andar " + andarDestino + "...");
+        while (andarAtual != andarDestino) {
+            if (andarDestino > andarAtual) {
+                andarAtual++;
+                System.out.println("Subindo... Andar: " + andarAtual);
+            } else {
+                andarAtual--;
+                System.out.println("Descendo... Andar: " + andarAtual);
+            }
+            try {
+                Thread.sleep(1000); // simula o tempo
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Chegou no andar " + andarAtual);
+    }
+
+    public void sair() {
+        if (pessoasPresentes > 0) {
+            pessoasPresentes--;
+            System.out.println("Uma pessoa saiu. Agora tem " + pessoasPresentes);
+        } else {
+            System.out.println("Elevador vazio.");
+        }
+    }
+
+    public void status() {
+        System.out.println("Andar atual: " + andarAtual);
+        System.out.println("Total de andares: " + totalAndares);
+        System.out.println("Capacidade: " + capacidade);
+        System.out.println("Pessoas presentes: " + pessoasPresentes);
+    }
 }
