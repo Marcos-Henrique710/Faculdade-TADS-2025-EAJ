c8c793e0175c6780a242357548cc7eeca6793fc8
Marcos_Henrique710 <marcoshenriqueufrn@gmail.com>
2025-09-09 09:40:24 -0300

    codigo exemplo imcompleto

---
+++ 2° periodo/programação orientada a objetos/atividades/atividades fixação POO/VerificarVogal/src/Main.java
@@ -0,0 +1,20 @@
+/*import java.lang.System;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String teste1 = "Estou na EAJ";
+    }
+    
+    private static int contaVogais(String text){
+        Pattern pattern = Patern.complete ("[aeiou]".Pattern.CASE_INSENSITIVE);
+
+    }
+    
+
+
+
+
+}
+*/
