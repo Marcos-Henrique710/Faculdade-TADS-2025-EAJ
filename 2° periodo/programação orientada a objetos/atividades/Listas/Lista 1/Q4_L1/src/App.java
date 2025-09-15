00cdfb53ec67cd6edb96eb7f153e20d6e35d0867
Marcos_Henrique710 <marcoshenriqueufrn@gmail.com>
2025-09-14 23:27:21 -0300

    atualização do codigo para apenas colocar o enunciado da questao

--- 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q4_L1/src/App.java
+++ 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q4_L1/src/App.java
@@ -1,3 +1,9 @@
+/*
+4.  Escreva  um  programa  que  dado  um  valor  numérico  digitado  pelo  usuário 
+(armazenado em   
+uma variável inteira), imprima cada um dos seus dígitos por extenso.   
+Exemplo:  ENTRADA: 4571, SAÍDA: quatro, cinco, sete, um
+*/
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
