b165166dce19d5066718f4575a849ed650536046
Marcos_Henrique710 <marcoshenriqueufrn@gmail.com>
2025-09-14 23:16:36 -0300

    atualização dos códigos das listas

--- 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q2_L1/src/Main.java
+++ 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q2_L1/src/Main.java
@@ -1,14 +1,69 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        String [] strTests = {"Paulo Jose de Almeida Prado", "Maria das Flores", "Lima da Silva" };
-        // etrada pode ser em qualquer fomato
-        String [] strResults = {" ", " ", " "};
-        for(int j =0; j < strTests[j].split(" ").length; j++) {
-            System.out.println("oloko");
+/* Crie uma classe que receba um nome completo na forma de uma String a partir do 
+console e mostre a abreviatura deste nome em maiúsculas. Não se devem abreviar 
+as palavras com 2 ou menos letras. A abreviatura deve vir separada por pontos. Ex: 
+Paulo  Jose  de  Almeida  Prado.  Abreviatura:  P. J. de A. P. Lembre do parâmetro 
+String  args[]  do  método  main().  Você  deve  testar  com  o  código  executado  no 
+terminal. Exemplo: se seu código é Abrevia.java, será: java Abrevia “Paulo da Silva 
+do Amaral”
+ */
+
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    String [] testCases = {
+      "Paulo Jose de Almeida Prado",
+      "Pedro Guilherme da Silva Cunha",
+      "Jaqueline Lima e Silva",
+      "Joana dos Santos",
+      "isis nicoly de souza"
+    };
+    
+    String [] testCasesExpected = {
+      "P. J. de A. P.",
+      "P. G. da S. C.",
+      "J. L. e S.",
+      "J. dos S.",
+      "I. N. de S."
+    };
+
+    String [] strAbbreviations = new String [testCases.length];
+    Arrays.fill(strAbbreviations, "");
+
+    int i = 0;
+    boolean toAbbreviate;
+    while (i < testCases.length) {
+      for (int j = 0; j < testCases[i].split(" ").length; j++) {
+        toAbbreviate = testCases[i].split(" ")[j].length() > 2 && !testCases[i].split(" ")[j].equals("dos") && !testCases[i].split(" ")[j].equals("das");
+        if (toAbbreviate) {
+          strAbbreviations[i] += testCases[i].split(" ")[j].toUpperCase().charAt(0) + ". "; 
+        } else {
+          strAbbreviations[i] += testCases[i].split(" ")[j] + " ";
         }
-        for(int i = 0; i < strTests.length; i++){
-            strResults[i] += strResults[i].charAt(0);
-        }
-        System.out.println("Hello, World!");
+      }
+      strAbbreviations[i] = strAbbreviations[i].trim();
+      i++;
     }
+    
+    printArray(strAbbreviations);
+    //imprimir resultado dos testes
+    for (int p = 0; p < strAbbreviations.length; p++) {
+      testAbbreviations(strAbbreviations[p], testCasesExpected[p]);
+    }
+    
+  } 
+ 
+  private static void printArray(String [] v) {
+    System.out.println("------------------------");
+    for (int i = 0; i < v.length; i++) {
+      System.out.println(v[i]);
+    }
+    System.out.println("------------------------");
+  }
+
+  private static void testAbbreviations(String a, String b) {
+    if (a.equals(b)) System.out.println("OK");
+    else System.out.println("FAIL");
+  }
+
 }
--- 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q3_L1/src/Main.java
+++ 2° periodo/programação orientada a objetos/atividades/Listas/Lista 1/Q3_L1/src/Main.java
@@ -9,8 +9,84 @@ e) Se a string digitada termina com “RIO” (ignorando maiúsculas/minúsculas
 f) O número de dígitos (0 a 9) da string.
 g) Se a string é um palíndromo ou não. 
 */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Digite uma string: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close(); 
+
+        // a) O número de caracteres da string.
+        int numChars = input.length();
+        System.out.println(numChars);
+
+        // b) A string com todas suas letras em maiúsculo.
+        String stringMaiuscula = input.toUpperCase();
+        System.out.println(stringMaiuscula);
+        
+        // c) O número de vogais da string.
+        int total = contaVogal(input);
+        System.out.println("Quantidade de vogais: " + total);
+        
+        //d) Se a string digitada começa com "UNI" (ignorando maiúscula/minúsculas).
+        if(input.startsWith("uni") || input.startsWith("UNI")) {
+            System.out.println("A string comeca com uni/UNI");
+        }else{
+            System.out.println("A string nao comeca com uni/UNI");
+        }
+        
+        //e) Se a string digitada termina com “RIO” (ignorando maiúsculas/minúsculas).
+        boolean verificarString = input.toLowerCase().endsWith(input.toLowerCase());
+        if(verificarString) {
+            System.out.println("A string termina com RIO/rio");
+        }else{
+            System.out.println("A string nao termina com RIO/rio");
+        }
+        
+        //f) O número de dígitos (0 a 9) da string.
+        int total2 = contarDigitos(input);
+        System.out.println("Quantidade de digitos(0 a 9) " + total2);
+        
+        //g) Se a string é um palíndromo ou não.
+         if (checarPalindromo(input)) {
+            System.out.println("A string eh um palindromo");
+        } else {
+            System.out.println("A string nao eh um palindromo");
+        }
+        
+    }
+
+    //d)
+    public static int contaVogal(String input) {
+        int contador = 0;
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) { 
+            char ch = input.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+    //f)
+    public static int contarDigitos(String input) {
+        int contador = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isDigit(ch)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+    //g)
+    public static boolean checarPalindromo(String str) {
+        String reversedString = new StringBuilder(str).reverse().toString();
+        return str.equals(reversedString);
     }
 }
