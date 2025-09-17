4cea0b5f8070eda6fb1287249c94a38a1dd84bd3
Marcos_Henrique710 <marcoshenriqueufrn@gmail.com>
2025-09-17 16:37:54 -0300

    Lista 2

---
+++ 2° periodo/programação orientada a objetos/atividades/Listas/Lista 2/Q1_L2/src/Main.java
@@ -0,0 +1,30 @@
+/*
+1.  Crie  uma  classe  TestArrayListNumber  com  o  método  main()  e  escreva  os  códigos  que 
+atendam aos requisitos abaixo:  
+a.  Crie  um  ArrayList  de  Integer,  com  a  notação  List<Integer>  list  =  new 
+ArrayList<Integer>(); 
+b.  Adicionar a este ArrayList 10 números inteiros aleatórios no intervalo [10, 20]. Verifique 
+como fazer isto com o método random() da classe Math (Math.random()) 
+c.  Verifique  se  há  números  repetidos  na  lista.  Se  houver  informe-os  e  quantas  vezes 
+aparecem na lista. Exemplo: list = {10,15,15,5,10,10,10,3,8,12}. Saída: {10: 4, 15:2} 
+i.  Dica: explore o método contains e o método indexOf 
+d.  Crie uma cópia desta lista com o método clone() 
+e.  Ordene em ordem decrescente esta lista clonada 
+f.  Remova o maior número da lista original 
+g.  Remova o maior número da lista clonada 
+h.  Una a duas listas, colocando a lista clonada no fim da lista original 
+i.  Verifique o tamanho da lista resultante 
+j.  Retorne  a  soma  dos  elementos  da  lista  resultante.  Pesquise  na  web  se  há  outras 
+formas de fazer esta soma para além do método clássico 
+k.  Substitua o menor (ou menores) números da lista resultante pelo valor médio da lista 
+l.  Crie  um  vetor  clássico Java preenchido com 5 valores inteiros e converta para lista. 
+Pesquisa o comando asList()... 
+m. Pesquise  sobre  a  collection  SET  em  Java.  Ela  permite  ordenação  e  valores 
+duplicados? Inclua um exemplo didático com Set para mostrar sua resposta. 
+*/
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
